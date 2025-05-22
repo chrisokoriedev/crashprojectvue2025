176dde989982f52d25ba4b1fae4d52f42008e054
chrisokoriedev <okoriec01@gmail.com>
2025-05-22 04:07:48 +0100

    update learn v-conditonal statement

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 export default {
   data() {
     return {
-      name: 'chris'
+      name: 'chris',
+      status: 'pending',
     }
   },
 }
 </script>
 <template>
   <h1> hello {{ name }}</h1>
+  <p v-if="status=='active'">user is active</p>
+  <p v-else-if="status=='pending'">user is pending</p>
+  <p v-else="status">user is inactive</p>
 </template>
