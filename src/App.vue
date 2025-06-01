847da74c49ddb5086aca5e7870b366bf3b0b8e54
chrisokoriedev <okoriec01@gmail.com>
2025-06-01 12:19:36 +0100

    feat: add JobListing component to App.vue
    - Import and include the JobListing component in App.vue.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
 import HomeCards from './components/HomeCards.vue';
+import JobListing from './components/JobListing.vue';
 </script>
 
 <template>
     <Navbar />
     <Hero />
     <HomeCards />
+    <JobListing />
 </template>
