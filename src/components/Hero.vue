4d18429cab0847b60cc61addb79e59912ea63695
chrisokoriedev <okoriec01@gmail.com>
2025-06-01 12:02:12 +0100

    feat: enhance hero section, add HomeCards component, and configure Tailwind CSS
    - Update Hero.vue to use font-bold instead of font-extrabold for the title.
    - Import and include the HomeCards component in App.vue.
    - Configure tailwind.config.js to disable darkMode.
    - Add a link to main.css in index.html.
    - Update main.css to import tailwindcss and enable smooth scrolling.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -15,7 +15,7 @@ defineProps({
     <section class="bg-green-700 py-20 mb-4">
         <div class="flex-col items-center">
             <div class="text-center">
-                <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+                <h1 class="text-4xl font-bold text-white sm:text-5xl md:text-6xl">
                     {{ title }}
                 </h1>
                 <p class="my-4 text-xl text-white">
