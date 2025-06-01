8c8a3d166b30add6aee18f82dd2dabaf43fc8393
chrisokoriedev <okoriec01@gmail.com>
2025-06-01 14:35:23 +0100

    feat: integrate PrimeIcons library for enhanced UI icons
    - Integrate the PrimeIcons library into the project.
    - Update the `package.json` and `package-lock.json` files to include `primeicons` as a dependency.
    - Import the `primeicons/primeicons.css` stylesheet in `src/main.js` to apply the icon styles.
    - Replace the Font Awesome location icon in `SingleJobListiing.vue` with the PrimeIcons equivalent (`pi pi-map-marker`).

--- src/components/SingleJobListiing.vue
+++ src/components/SingleJobListiing.vue
@@ -42,7 +42,7 @@ const truncatedDescription = computed(() => {
 
             <div class="flex flex-col lg:flex-row justify-between mb-4">
                 <div class="text-orange-700 mb-3">
-                    <i class="fa-solid fa-location-dot text-lg"></i>
+                    <i class="pi pi-map-marker text-orange-500 text-lg"></i>
                     {{ job.location }}
                 </div>
                 <a :href="'/job/' + job.id" class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'primeicons/primeicons.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
 createApp(App).mount('#app')
+ 
