03395fc7dad3c8de26b8db67905d75747ddaf491
chrisokoriedev <okoriec01@gmail.com>
2025-06-01 14:57:46 +0100

    feat: set up Vue Router and Home view
    - Install and configure Vue Router for navigation.
    - Create a basic `Home` view component.
    - Define a route for the home page ("/").
    - Mount the router instance to the Vue app.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
 import 'primeicons/primeicons.css'
 
 
-createApp(App).mount('#app')
+const app=createApp(App)
+app.use(router);
+app.mount('#app');
  
