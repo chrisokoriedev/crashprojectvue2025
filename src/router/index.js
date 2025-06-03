72a06934978568d07862d69bb9653219c885694f
chrisokoriedev <okoriec01@gmail.com>
2025-06-03 15:06:07 +0100

    feat: add ViewJob component and update routing for job details

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from "@/views/Home.vue";
 import Jobs from "@/views/jobs.vue";
 import AddJob from "@/views/addjob.vue";
 import NotFound from "@/views/NotFound.vue";
+import ViewJob from "@/views/ViewJob.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,6 +16,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: Jobs,
+    },
+    {
+      path: "/viewJob/:id",
+      name: "viewJob",
+      component: ViewJob,
     },
     {
       path: "/addjob",
